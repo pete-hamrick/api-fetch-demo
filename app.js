d031db2a387deef6ea3b1944563bf02be9328f57
Pete Hamrick <pete.hamrick@gmail.com>
2022-03-03 11:44:59 -0800

    grabbed dom elements in app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // import functions
 
 // grab DOM elements
-
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+const template = document.querySelector('#template');
+const selectEl = document.querySelector('select');
+const list = document.querySelector('#list');
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
