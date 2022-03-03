6ca8f9453ba8b2e971d8580359edb39e5a486700
Pete Hamrick <pete.hamrick@gmail.com>
2022-03-03 11:55:28 -0800

    added loadPokedex function to app.js

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 // import functions
-
+import { getPokedex } from './fetch-utils.js';
 // grab DOM elements
 const template = document.querySelector('#template');
 const selectEl = document.querySelector('select');
 const list = document.querySelector('#list');
+
+async function loadPokedex() {
+    const pokedex = await getPokedex();
+
+    list.classList.add('pokemon');
+
+    for (let pokemon of pokedex) {
+        const clone = template.textContent.cloneNode(true);
+
+        const name = clone.querySelector('h2');
+        const image = clone.querySelector('img');
+        const type = clone.querySelector('h6');
+
+        name.textContent = 'Name: ' + pokemon.pokemon;
+
+        type.textContent = 'Type: ' + pokemon.type_1;
+
+        image.src = pokemon.url_image;
+        image.alt = pokemon.pokemon;
+
+        list.appendChild(clone);
+    }
+}
+
 // set event listeners
 // get user input
 // use user input to update state
