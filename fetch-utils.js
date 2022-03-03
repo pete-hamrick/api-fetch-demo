089d40098fbee992a605a3db1fba3646b96b0924
Pete Hamrick <pete.hamrick@gmail.com>
2022-03-03 11:39:19 -0800

    wrote my async getPokedex function

---
+++ fetch-utils.js
@@ -0,0 +1,10 @@
+export async function getPokedex() {
+    let url = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+    // console.log(url + '/fightingType');
+
+    const response = await fetch(url);
+
+    const jsonResponse = await response.json();
+
+    return json.results;
+}
